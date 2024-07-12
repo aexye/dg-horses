0e57d090e73590bc5561ed9d9458a51149c8bd51
Maciej Ostrowski <93351967+aexye@users.noreply.github.com>
2024-07-12 15:43:11 +0200

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.set_page_config(page_title="Main page", page_icon="👋")
-st.logo("st-website\dg-logo.png")
+st.logo("dg-logo.png")
 # @st.cache_resource
 # def init_connection():
 #     url = st.secrets["SUPABASE_URL"]

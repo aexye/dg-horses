4418664e4be2e3bd425bc85964364485f6b1292c
Maciej Ostrowski <93351967+aexye@users.noreply.github.com>
2024-08-02 17:24:52 +0200

    Update United_Kingdom.py

--- pages/United_Kingdom.py
+++ pages/United_Kingdom.py
@@ -31,7 +31,7 @@ def get_data_uk():
         response_uk = supabase.table('uk_horse_racing_full').select('race_date', 'race_name', 'city', 'horse', 'jockey', 'odds', 'odds_predicted', 'num', 'positive_hint', 'negative_hint', 'draw').execute()
         df = pd.DataFrame(response_uk.data)
         df['race_date'] = pd.to_datetime(df['race_date'])
-        df.rename(columns={'horse': 'Horse', 'jockey': 'Jockey', 'odds_predicted': 'Odds predicted', 'num': 'Horse number', 'odds': 'Initial market odds', 'positive_hint': 'Betting hint (+)', 'negative_hint': 'Betting hint (-)', 'draw_norm': 'Draw'}, inplace=True)
+        df.rename(columns={'horse': 'Horse', 'jockey': 'Jockey', 'odds_predicted': 'Odds predicted', 'num': 'Horse number', 'odds': 'Initial market odds', 'positive_hint': 'Betting hint (+)', 'negative_hint': 'Betting hint (-)', 'draw': 'Draw'}, inplace=True)
         return df
     except Exception as e:
         st.error(f"Error fetching data from Supabase: {e}")

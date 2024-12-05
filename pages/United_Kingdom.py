39cf27ea5bbed513c492234db8a4da262bc1247a
Maciej Ostrowski <93351967+aexye@users.noreply.github.com>
2024-12-05 15:13:31 +0100

    Update United_Kingdom.py

--- pages/United_Kingdom.py
+++ pages/United_Kingdom.py
@@ -1,3 +1,67 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from supabase import create_client, Client
+from google.oauth2 import service_account
+from google.cloud import bigquery
+import plotly.express as px
+
+st.set_page_config(page_title="UK Horse Racing", page_icon="🇬🇧", layout="wide")
+st.logo("dg-logo.png")
+# Initialize clients (consider moving this to a separate function)
+@st.cache_resource
+def init_clients():
+    url = st.secrets["supabase_url"]
+    key = st.secrets["supabase_key"]
+    supabase: Client = create_client(url, key)
+    
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
+    )
+    bq_client = bigquery.Client(credentials=credentials)
+    
+    return supabase, bq_client
+
+supabase, bq_client = init_clients()
+
+# Fetch data from Supabase
+@st.cache_data(ttl=600)
+def get_data_uk():
+    try:
+        response_gb = supabase.table('uk_horse_racing_full').select('race_date', 'race_id', 'horse_id', 'race_name', 'city', 'horse', 'jockey','odds', 'odds_predicted', 'horse_num', 'positive_hint', 'draw_norm', 'last_5_positions', 'odds_predicted_intial', 'winner_prob','trifecta_prob','quinella_prob','place_prob','last_place_prob').execute()
+        df = pd.DataFrame(response_gb.data)
+        df['race_date'] = pd.to_datetime(df['race_date'])
+        df.rename(columns={'horse': 'Horse', 'jockey': 'Jockey', 'odds_predicted': 'Odds predicted', 'horse_num': 'Horse number', 'odds': 'Initial market odds', 'positive_hint': 'Betting hint', 
+                            'last_5_positions': 'Last 5 races', 'draw_norm': 'Draw', 'odds_predicted_intial': 'Odds predicted (raw)',
+                           'winner_prob': 'Win probability','trifecta_prob': 'Top3 probability','quinella_prob': 'Top2 probability','last_place_prob': 'Last place probability'
+                            }, inplace=True)
+        return df
+    except Exception as e:
+        st.error(f"Error fetching data from Supabase: {e}")
+        return pd.DataFrame()
+
+# Fetch data from BigQuery
+@st.cache_data(ttl=600)
+def get_bigquery_data():
+    query = "SELECT * FROM `data-gaming-425312.gb_horse_data.gb_data__predictions_stats`"
+    try:
+        df = bq_client.query(query).to_dataframe()
+        df['race_date'] = pd.to_datetime(df['race_date'])
+        return df
+    except Exception as e:
+        st.error(f"Error fetching data from BigQuery: {e}")
+        return pd.DataFrame()
+
+@st.cache_data(ttl=600)
+def get_bigquery_odds_data():
+    try:
+        query = "SELECT * FROM `data-gaming-425312.gb_horse_data.gb_horse_odds`"
+        df = bq_client.query(query).to_dataframe()
+        return df
+    except Exception as e:
+        st.error(f"Error fetching data from BigQuery: {e}")
+        return pd.DataFrame()
+
 def display_race_data(df, odds_df):
     st.subheader("Race Data")
     print(df.head())
@@ -107,3 +171,37 @@ def display_race_data(df, odds_df):
             st.markdown("---")  # Add a separator between races
     else:
         st.info("Please select at least one race name to display the data.")
+
+def plot_accuracy(df):
+    st.subheader("Accuracy metric")
+    st.markdown("Accuracy over time metric - in other words, how well our model is predicting the top 3 finishers in each race. It is NOT the accuracy of the odds or overall accuracy of the model.")
+    fig = px.bar(df, x='race_date', y='avg_acc_top3', title='Average Accuracy (Top 3)', labels={'avg_acc_top3': 'Accuracy', 'race_date': 'Date'})
+    st.plotly_chart(fig, use_container_width=True)
+
+def plot_earnings(df):
+    st.subheader("Cumulative Earnings")
+    st.markdown("This chart illustrates the earnings that would have resulted from betting $10 on the top 1 finisher in each race, using the closing odds to determine the payout. The chart shows the total amount of money that would have been earned if this strategy had been employed.")
+    fig = px.bar(df, x='race_date', y='money_earned_top1', title='Cumulative Sum Earned (Top 1)', labels={'money_earned_top1': 'Earnings over time in $', 'race_date': 'Date'})
+    st.plotly_chart(fig, use_container_width=True)
+
+def main():
+    st.title("🇬🇧 UK Horse Racing Odds Prediction")
+    
+    tab1, tab2 = st.tabs(["Race Data", "Performance Metrics"])
+    
+    with tab1:
+        race_data = get_data_uk()
+        odds_data = get_bigquery_odds_data()
+        display_race_data(race_data, odds_data)
+    
+    with tab2:
+        bq_data = get_bigquery_data()
+        col1, col2 = st.columns(2)
+        with col1:
+            plot_accuracy(bq_data)
+        with col2:
+            plot_earnings(bq_data)
+        # st.dataframe(bq_data)
+
+if __name__ == "__main__":
+    main()
